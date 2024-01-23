9396ad5645c6ab79759f7785b778fb5a2c16160c
Christian Lewe <ch_lewe@agdsn.me>
2024-01-23 14:47:13 +0100

    HTML: Remove HTML generation from Rust

--- src/lib.rs
+++ src/lib.rs
@@ -3,22 +3,5 @@ pub mod foo;
 
 extern crate libc;
 
-use wasm_bindgen::prelude::*;
-
 #[cfg(test)]
 wasm_bindgen_test::wasm_bindgen_test_configure!(run_in_browser);
-
-// Called when the wasm module is instantiated
-#[wasm_bindgen(start)]
-fn start() -> Result<(), JsValue> {
-    let window = web_sys::window().expect("no global `window` exists");
-    let document = window.document().expect("should have a document on window");
-    let body = document.body().expect("document should have a body");
-
-    let val = document.create_element("p")?;
-    val.set_inner_html("Hello from Rust!");
-
-    body.append_child(&val)?;
-
-    Ok(())
-}
